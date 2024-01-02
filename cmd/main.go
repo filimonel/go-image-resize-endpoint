1793a0df0c860873451e14a781e5834f6783f5a8
filimonel <filimone.dev@gmail.com>
2024-01-02 21:04:01 +1100

    update routes - add rate limit

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/filimonel/go-image-resize-endpoint/internal/handlers"
 	"github.com/filimonel/go-image-resize-endpoint/internal/middleware"
+	ratelimiters "github.com/filimonel/go-image-resize-endpoint/internal/rate-limiters"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/rs/cors"
+	"golang.org/x/time/rate"
 )
 
 var ginLambda *ginadapter.GinLambda
@@ -21,12 +25,20 @@ func init() {
 	// Set Cors
 	r.Use(cors.New(middleware.SetCors()))
 
+	// Set memory limit for multipart forms
+	r.MaxMultipartMemory = 2 << 20
+
+	// Define the rate limit rules
+	limiter := rate.NewLimiter(rate.Every(time.Hour), 5)
+
 	// Routes & Handlers
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello, world!",
 		})
 	})
+
+	r.POST("/upload", ratelimiters.SetLimit(limiter), handlers.GenerateImageSizesHandler)
 
 	ginLambda = ginadapter.New(r)
 }
