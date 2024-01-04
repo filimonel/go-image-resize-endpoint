83a385b05d6cd8b45b845934f5ba4ca55f21416a
filimonel <filimone.dev@gmail.com>
2024-01-04 17:44:15 +1100

    [handler] add more image sizes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -15,7 +15,7 @@ func GenerateImageSizesHandler(c *gin.Context) {
 	file, _, err := c.Request.FormFile("image")
 	if err != nil {
 		log.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Bad request", "error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request"})
 		return
 	}
 	defer file.Close()
@@ -23,12 +23,14 @@ func GenerateImageSizesHandler(c *gin.Context) {
 	img, _, err := image.Decode(file)
 	if err != nil {
 		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error decoding image", "error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding image"})
 		return
 	}
 
-	img1 := imaging.Resize(img, 800, 600, imaging.Lanczos)
-	img2 := imaging.Resize(img, 400, 300, imaging.Lanczos)
+	img1 := imaging.Resize(img, 150, 150, imaging.Lanczos)
+	img2 := imaging.Resize(img, 800, 600, imaging.Lanczos)
+	img3 := imaging.Resize(img, 1280, 720, imaging.Lanczos)
+	img4 := imaging.Resize(img, 1920, 1080, imaging.Lanczos)
 
 	buf1 := new(bytes.Buffer)
 	_ = imaging.Encode(buf1, img1, imaging.JPEG)
@@ -38,7 +40,15 @@ func GenerateImageSizesHandler(c *gin.Context) {
 	_ = imaging.Encode(buf2, img2, imaging.JPEG)
 	img2Base64 := base64.StdEncoding.EncodeToString(buf2.Bytes())
 
+	buf3 := new(bytes.Buffer)
+	_ = imaging.Encode(buf3, img3, imaging.JPEG)
+	img3Base64 := base64.StdEncoding.EncodeToString(buf3.Bytes())
+
+	buf4 := new(bytes.Buffer)
+	_ = imaging.Encode(buf4, img4, imaging.JPEG)
+	img4Base64 := base64.StdEncoding.EncodeToString(buf4.Bytes())
+
 	c.JSON(http.StatusOK, gin.H{
-		"images": []string{img1Base64, img2Base64},
+		"images": []string{img1Base64, img2Base64, img3Base64, img4Base64},
 	})
 }
