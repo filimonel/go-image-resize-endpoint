c41cf45740c2770bfdb705993647a2dd334112fb
filimonel <filimone.dev@gmail.com>
2024-01-05 13:04:51 +1100

    [update] reconfigure handler to accept and process png images

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,16 +3,20 @@ package handlers
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
 	"image"
+	"image/jpeg"
+	"image/png"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
 )
 
 func GenerateImageSizesHandler(c *gin.Context) {
-	file, _, err := c.Request.FormFile("image")
+	file, header, err := c.Request.FormFile("image")
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request"})
@@ -32,21 +36,36 @@ func GenerateImageSizesHandler(c *gin.Context) {
 	img3 := imaging.Resize(img, 1280, 720, imaging.Lanczos)
 	img4 := imaging.Resize(img, 1920, 1080, imaging.Lanczos)
 
-	buf1 := new(bytes.Buffer)
-	_ = imaging.Encode(buf1, img1, imaging.JPEG)
-	img1Base64 := base64.StdEncoding.EncodeToString(buf1.Bytes())
+	encodeImage := func(img image.Image, format string) (string, error) {
+		buf := new(bytes.Buffer)
+		switch format {
+		case "jpeg", "jpg":
+			if err := jpeg.Encode(buf, img, nil); err != nil {
+				return "", err
+			}
+		case "png":
+			if err := png.Encode(buf, img); err != nil {
+				return "", err
+			}
+		default:
+			return "", fmt.Errorf("unsupported format: %s", format)
+		}
+		return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
+	}
 
-	buf2 := new(bytes.Buffer)
-	_ = imaging.Encode(buf2, img2, imaging.JPEG)
-	img2Base64 := base64.StdEncoding.EncodeToString(buf2.Bytes())
+	formats := strings.Split(header.Filename, ".")
+	imgFormat := formats[len(formats)-1]
 
-	buf3 := new(bytes.Buffer)
-	_ = imaging.Encode(buf3, img3, imaging.JPEG)
-	img3Base64 := base64.StdEncoding.EncodeToString(buf3.Bytes())
+	img1Base64, err := encodeImage(img1, imgFormat)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error encoding image"})
+		return
+	}
 
-	buf4 := new(bytes.Buffer)
-	_ = imaging.Encode(buf4, img4, imaging.JPEG)
-	img4Base64 := base64.StdEncoding.EncodeToString(buf4.Bytes())
+	img2Base64, _ := encodeImage(img2, imgFormat)
+	img3Base64, _ := encodeImage(img3, imgFormat)
+	img4Base64, _ := encodeImage(img4, imgFormat)
 
 	c.JSON(http.StatusOK, gin.H{
 		"images": []string{img1Base64, img2Base64, img3Base64, img4Base64},
